36d7addf1766ec4e4c80fccf861c106bdf9ac7a2
nunocsalvador <nuno.c.salvador@gmail.com>
2016-09-22 18:10:34 +0100

    manual

--- SIMH_Manual_Utilizador/js/script.js
+++ SIMH_Manual_Utilizador/js/script.js
@@ -32,8 +32,8 @@ $('tr').each(function(){
  function zommClickImagem() {
 	 $('#paginas p>img:not([alt="logo"])').each(function(){
 		 var alt = $(this).attr("alt")
-		 if(alt != "figAlteracaoSenha" && alt != "figLogin" && alt !="figLoginRecuperacao"
-		   && alt != "figAcessoGestaoUtilizadores" && alt != "figAcessoCodificacaoEpisodios" && alt != "figAcessoMedicosCodificadores")
+		 if(alt != "figAlteracaoSenha" && alt != "figLogin" && 
+		    alt != "figAcessoGestaoUtilizadores" && alt != "figAcessoCodificacaoEpisodios" && alt != "figAcessoMedicosCodificadores")
 		 $(this).wrap("<a class='imagem' href='"+$(this).attr( "src" ) + "' onclick='return hs.expand(this)'></a>"); 
 });
 }
